e94e5e9b8fcd7d835bfe23804d5a9a7616a3e4a4
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-12 15:18:31 +0200

    feat: AppPosts modificata

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import axios from 'axios'
 import AppHome from './pages/AppHome';
 import AppPosts from './pages/AppPosts';
 import AppAboutUs from './pages/AppAboutUs';
 import GuestLayout from './layouts/GuestLayout';
 
 function App() {
-  const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts ";
-  const [datiPost, setDatiPost] = useState([])
-useEffect(() => {
-  axios.get(apiUrl).then((resp) => {
-    {setDatiPost(resp.data)}
-  })
-}, [])
   return (
     <>
     <BrowserRouter>
--- src/pages/AppPosts.jsx
+++ src/pages/AppPosts.jsx
@@ -1,6 +1,29 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const AppPosts = () => {
+    const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+  const [datiPost, setDatiPost] = useState([])
+useEffect(() => {
+  axios.get(apiUrl).then((resp) => {
+    setDatiPost(resp.data)
+  })
+}, [])
 return (
-    <p>lista post</p>
+    <>
+<h1 className='text-center my-4'>Dai uno sguardo ai nostri ultimi posts</h1>
+<div className="container">
+    <div className="row">
+   {datiPost.map((curPost, index) => (
+  <div className="card my-3 col-3 p-3 m-2 ms-auto">
+  <div className="card-body" key={index}>
+    <h3 className="card-title">{curPost.title}</h3>
+    <h6 className="card-subtitle mb-2 text-body-secondary">{curPost.author}</h6>
+    <p className="card-text">{curPost.body}</p>
+  </div>
+ </div>
+   )) }
+</div></div></>
 )
 }
 export default AppPosts
