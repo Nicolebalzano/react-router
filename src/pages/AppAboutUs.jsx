a16d103b56904eaa90236d42541389d2b0a1fc98
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-12 15:57:24 +0200

    feat: p aggiunto a about us

--- src/pages/AppAboutUs.jsx
+++ src/pages/AppAboutUs.jsx
@@ -3,6 +3,10 @@ return (
     <div className="container text-center">
     <h3 className="text-center my-5 summery-title">Find out more about us</h3>
     <img src="/AboutUsSummer.png" alt="" />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus omnis voluptatibus saepe, delectus temporibus similique deleniti repudiandae repellendus fugiat ipsam aut molestias nulla cupiditate eveniet perferendis dolorem nostrum nobis suscipit!
+    Animi veritatis consequatur provident incidunt aut, iste dolores alias enim, porro corporis delectus accusantium ullam quam magnam non debitis, nemo saepe! Iste doloribus beatae cupiditate perferendis ad? Optio, tenetur pariatur!
+    Commodi, aliquam? Assumenda nihil maiores soluta non veritatis obcaecati a nulla commodi, nemo autem voluptatibus aut quo beatae reprehenderit! Labore saepe minus cupiditate nulla fuga tempore sint temporibus architecto voluptatem?
+    Obcaecati eligendi, porro optio quidem id architecto dolor error earum recusandae rem officia iure necessitatibus, vel sapiente provident. Impedit, nihil debitis nisi consequatur tempora nesciunt similique illum perferendis saepe perspiciatis!</p>
     </div>
 )
 }
